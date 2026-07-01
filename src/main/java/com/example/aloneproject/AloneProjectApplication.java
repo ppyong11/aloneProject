3dbd628448f10cf407f2c7993b15edef023e949c
김아현 <kah1722291@gmail.com>
2026-07-02 08:32:01 +0900

    new file: KIM-5 이슈 자동 처리 테스트

---
+++ src/main/java/com/example/aloneproject/AloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.aloneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AloneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AloneProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aloneproject/AloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aloneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AloneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
